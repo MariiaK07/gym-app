0de3e36bc72053da6c9ca3d6868f78a28a5c4fa0
Mariia Kovach <mariiakovach123@gmail.com>
2023-01-14 22:24:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gym Application
+
+**[Gym App Demo](https://mariiak07.github.io/gym-app/)**
+
+**`STACK:` React, TS, Vite, Tailwind, Hero Icons, Framer Motion, Form Submit, React Anchor Link Smooth Scroll**
---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,39 @@
+import { SelectedPage } from '@/shared/types';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+  setIsMenuToggled?: (value: boolean) => void;
+};
+
+const Link = ({
+  page,
+  selectedPage,
+  setSelectedPage,
+  setIsMenuToggled,
+}: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, '') as SelectedPage;
+
+  const handleClick = () => {
+    setSelectedPage(lowerCasePage);
+    setIsMenuToggled && setIsMenuToggled(false);
+  };
+
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? 'text-primary-500' : ''}
+        transition
+        duration-500
+        hover:text-primary-300
+      `}
+      href={`#${lowerCasePage}`}
+      onClick={handleClick}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
